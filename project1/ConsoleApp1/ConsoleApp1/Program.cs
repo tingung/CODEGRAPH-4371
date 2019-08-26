ca416f9361b123fb89ea621acb6b280557a86802
tingung <tingung.hu@aurea.com>
2019-08-26 10:16:10 +0800

    First commit

---
+++ project1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public bool test = true;
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ project2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public bool test = true;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
